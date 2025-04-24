c5c6e495c04c69614eb8e922a8f4cd5269080cb3
Ilia <kzerby@gmail.com>
2025-04-24 11:36:02 -0600

    tee

--- .devpanel/init.sh
+++ .devpanel/init.sh
@@ -38,7 +38,7 @@ composer require --dev drush/drush
 
 #== AWS configure.
 sudo mkdir -p /.aws/
-sudo cat <<EOT > /.aws/credentials
+sudo tee /.aws/credentials > /dev/null <<EOT
 [performantlabs]
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
