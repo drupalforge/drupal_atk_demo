7fded2003cdc9ef40e6187789f68ebb1b68542d1
M Miller <melissa.miller@pantheon.io>
2025-05-01 11:13:00 -0700

    More work on test runner form
    Moving to use Drupal FAPI

--- web/modules/custom/pl_drupal_forge/src/Controller/AtkClientController.php
+++ web/modules/custom/pl_drupal_forge/src/Controller/AtkClientController.php
@@ -3,15 +3,34 @@
 namespace Drupal\pl_drupal_forge\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Form\FormBuilderInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use GuzzleHttp\Client;
 
 
 class AtkClientController extends ControllerBase {
+  /**
+   * The form builder service.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Constructs a StartPageController object.
+   *
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder service.
+   */
+  public function __construct(FormBuilderInterface $form_builder) {
+    $this->formBuilder = $form_builder;
+  }
 
   public static function create(ContainerInterface $container) {
-    return new static();
+    return new static(
+      $container->get('form_builder')
+    );
   }
 
   /**
@@ -23,8 +42,12 @@ class AtkClientController extends ControllerBase {
    * @return array A simple renderable array.
    */
   public function start() {
+    // Build the form render array.
+    $form = $this->formBuilder->getForm('Drupal\pl_drupal_forge\Form\TestRunnerForm');
+
     return [
       '#theme' => 'start_page_template',
+      '#start_test_form' => $form,
       '#base_url' => $this->getBaseUrl(),
       '#tags' => $this->config('pl_drupal_forge.settings')->get('tags'),
     ];
---
+++ web/modules/custom/pl_drupal_forge/src/Form/TestRunnerForm.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\pl_drupal_forge\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a custom form for the test runner.
+ */
+class TestRunnerForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'test_runner_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['#attributes'] = [
+      'class' => ['pl-form'],
+    ];
+
+    $form['url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('URL'),
+      '#default_value' => \Drupal::request()->getSchemeAndHttpHost(),
+      '#required' => TRUE,
+      '#attributes' => [
+        'class' => ['pl-form-item__input'],
+      ],
+      "#label_attributes" => [
+        'class' => ['pl-form-item__label'],
+      ],
+      '#wrapper_attributes' => [
+        'class' => ['pl-form-item'],
+      ],
+    ];
+
+    $form['tags'] = [
+      '#type' => 'multiselect_dropdown',
+      '#title' => $this->t('Tags'),
+      '#options' => $this->getTags(),
+      '#default_open' => FALSE,
+      '#modal_breakpoint' => 760,
+      '#label_aria' => $this->t('Toggle the list of tags'),
+      '#label_none' => $this->t('Select tags...'),
+      '#label_all' => $this->t('All tags'),
+      '#label_single' => $this->t('%d tag selected'),
+      '#label_plural' => $this->t('%d tags selected'),
+      '#label_close' => $this->t('Close'),
+      '#label_select_all' => $this->t('Select all'),
+      '#label_select_none' => $this->t('Select none'),
+      '#attributes' => [
+        'class' => ['pl-form-item__input'],
+      ],
+      "#label_attributes" => [
+        'class' => ['pl-form-item__label'],
+      ],
+      '#wrapper_attributes' => [
+        'class' => ['pl-form-item'],
+      ],
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Start Tests'),
+      '#attributes' => [
+        'id' => 'the-button',
+        'class' => ['pl-button', 'pl-button--lg'],
+        'onclick' => 'invoke();',
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // This function is intentionally left empty.
+    // The form submission is handled via JavaScript.
+  }
+
+  /**
+   * Helper function to get tags.
+    */
+  private function getTags() {
+    // Fetch tags from configuration.
+    $config = $this->config('pl_drupal_forge.settings');
+    $tags = $config->get('tags') ?? [];
+
+    // Ensure the tags are in the correct format for the form.
+    return array_combine($tags, $tags);
+  }
+
+}
